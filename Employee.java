da9766f1f0cc7c48b1a83fe26dcebecaeab06acf
Reshma-Immaculate <64631715+Reshma-Immaculate@users.noreply.github.com>
2021-04-11 23:39:09 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,27 @@
+
+public class Employee {
+String name;
+int year;
+long salary;
+String address;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee obj1 = new Employee();
+		Employee obj2 = new Employee();
+		Employee obj3 = new Employee();
+		System.out.println(obj1.name="Robert");
+		System.out.println(obj1.year=1994);
+		System.out.println(obj1.salary=1200000);
+		System.out.println(obj1.address="64C - Wall Street");
+		System.out.println(obj2.name="Sam");
+		System.out.println(obj2.year=2000);
+		System.out.println(obj2.salary=3600000);
+		System.out.println(obj2.address="68D - Wall Street");
+		System.out.println(obj3.name="John");
+		System.out.println(obj3.year=1994);
+		System.out.println(obj3.salary=5400000);
+		System.out.println(obj3.address="26B - Wall Street");
+
+	}
+
+}
